ff225b12c63632797fe716f3f85dc82c28d5f2e1
Nemo <me@captnemo.in>
2020-04-08 00:55:59 +0530

    Fix filenames with double-quotes

--- src/util.cr
+++ src/util.cr
@@ -2,7 +2,7 @@ module Muse::Dl
   class Util
     # Generates a safe filename
     def self.slug_filename(input : String)
-      input.strip.tr("\u{202E}%$|:;/\t\r\n\\", "-")
+      input.strip.tr("\u{202E}%$|:;/\"\t\r\n\\", "-")
     end
   end
 end
