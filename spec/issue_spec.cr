3e56efed52f5cfd2cbd0efd32ded9438c82358f2
Nemo <me@captnemo.in>
2020-04-08 01:52:28 +0530

    Parses summary for issueS

--- spec/issue_spec.cr
+++ spec/issue_spec.cr
@@ -22,11 +22,11 @@ describe Muse::Dl::Issue do
     issue.title.should eq "Volume 20, Number 1, January 2020"
   end
 
-  # it "should parser summary" do
-  #   issue.summary.should eq <<-EOT
-  #   Focusing on important research about the role of academic libraries and librarianship, portal also features commentary on issues in technology and publishing. Written for all those interested in the role of libraries within the academy, portal includes peer-reviewed articles addressing subjects such as library administration, information technology, and information policy. In its inaugural year, portal earned recognition as the runner-up for best new journal, awarded by the Council of Editors of Learned Journals (CELJ). An article in  portal, "Master's and Doctoral Thesis Citations: Analysis and Trends of a Longitudinal Study," won the Jesse H. Shera Award for Distinguished Published Research from the Library Research Round Table of the American Library Association.
-  #   EOT
-  # end
+  it "should parser summary" do
+    issue.summary.should eq <<-EOT
+    Focusing on important research about the role of academic libraries and librarianship, portal also features commentary on issues in technology and publishing. Written for all those interested in the role of libraries within the academy, portal includes peer-reviewed articles addressing subjects such as library administration, information technology, and information policy. In its inaugural year, portal earned recognition as the runner-up for best new journal, awarded by the Council of Editors of Learned Journals (CELJ). An article in  portal, "Master's and Doctoral Thesis Citations: Analysis and Trends of a Longitudinal Study," won the Jesse H. Shera Award for Distinguished Published Research from the Library Research Round Table of the American Library Association.
+    EOT
+  end
 
   it "should parse publisher" do
     issue.publisher.should eq "Johns Hopkins University Press"
